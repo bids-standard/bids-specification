980894f75bdd685b03eff2fcf7dd237b552a37b0
Arshitha <arsh2794@gmail.com>
2020-02-21 15:45:14 -0500

    cleaned up code and final checks with circleci

--- pdf_build_src/process_markdowns.py
+++ pdf_build_src/process_markdowns.py
@@ -1,5 +1,4 @@
 """Process the markdown files.
-
 The purpose of the script is to create a duplicate src directory within which
 all of the markdown files are processed to match the specifications of building
 a pdf from multiple markdown files using the pandoc library (***add link to
@@ -10,6 +9,7 @@ well.
 import os
 import subprocess
 import re
+from datetime import datetime
 
 
 def run_shell_cmd(command):
@@ -32,59 +32,58 @@ def copy_src():
     run_shell_cmd(mkdir_cmd)
 
     # copy contents of src directory
-    copy_cmd = "cp -a "+src_path+" "+target_path
+    copy_cmd = "cp -R "+src_path+" "+target_path
     run_shell_cmd(copy_cmd)
 
 
 def copy_bids_logo():
     """Copy BIDS_logo.jpg from the BIDS_logo dir in the root of the repo."""
-    run_shell_cmd("cp ../BIDS_logo/BIDS_logo.jpg src_copy/images/")
+    run_shell_cmd("cp ../BIDS_logo/BIDS_logo.jpg src_copy/src/images/")
 
 
 def copy_images(root_path):
     """Copy images.
-
     Will be done from images directory of subdirectories to images directory
     in the src directory
     """
     subdir_list = []
 
     # walk through the src directory to find subdirectories named 'images'
-    # and copy contents to the 'images' directory in the duplicate src directory
+    # and copy contents to the 'images' directory in the duplicate src 
+    # directory
     for root, dirs, files in os.walk(root_path):
         if 'images' in dirs:
             subdir_list.append(root)
 
     for each in subdir_list:
         if each != root_path:
-            run_shell_cmd("cp -a "+each+"/images/"+" "+root_path+"/images/")
+            run_shell_cmd("cp -R "+each+"/images"+" "+root_path+"/images/")
 
 
 def extract_header_string():
     """Extract the latest release's version number and date from CHANGES.md."""
     released_versions = []
+    run_shell_cmd("cp ../mkdocs.yml src_copy/")
 
-    for i, line in enumerate(open('./src_copy/CHANGES.md')):
+    with open(os.path.join(os.path.dirname(__file__), 'src_copy/mkdocs.yml'), 'r') as file:
+        data = file.readlines()
 
-        match_list = re.findall(r'^##\s\[v.+\]', line)
+    header_string = data[0].split(": ")[1]
+    
+    title = " ".join(header_string.split()[0:4])
+    version_number = header_string.split()[-1]
+    build_date = datetime.today().strftime('%Y-%m-%d')
 
-        if len(match_list) > 0:
-            wordlist = line.split()
-            released_versions.append([match_list[0].split()[1], wordlist[2]])
-
-    version_number = released_versions[0][0].strip('[]')
-    version_date = released_versions[0][1].strip('()')
-
-    return version_number, version_date
+    return title, version_number, build_date
 
 
 def add_header():
     """Add the header string extracted from changelog to header.tex file."""
-    version_number, version_date = extract_header_string()
+    title, version_number, build_date = extract_header_string()
+    header = " ".join([title, version_number, build_date])
 
     # creating a header string with latest version number and date
-    header_string = ("\chead{Brain Imaging Data Structure " +
-                     version_number + " " + version_date + "}")
+    header_string = ("\chead{ " + header + " }")
 
     with open('header.tex', 'r') as file:
         data = file.readlines()
@@ -99,8 +98,7 @@ def add_header():
 
 def remove_internal_links(root_path, link_type):
     """Find and replace all cross and same markdown internal links.
-
-    The links will be replaced with plain text associated with the link.
+    The links will be replaced with plain text associated with it.
     """
     if link_type == 'cross':
         # regex that matches cross markdown links within a file
@@ -131,42 +129,41 @@ def remove_internal_links(root_path, link_type):
 
 def modify_changelog():
     """Change first line of the changelog to markdown Heading 1.
-
     This modification makes sure that in the pdf build, changelog is a new
     chapter.
     """
-    with open('./src_copy/CHANGES.md', 'r') as file:
+    with open('src_copy/src/CHANGES.md', 'r') as file:
         data = file.readlines()
 
     data[0] = "# Changelog"
 
-    with open('./src_copy/CHANGES.md', 'w') as file:
+    with open('src_copy/src/CHANGES.md', 'w') as file:
         file.writelines(data)
 
 
 def edit_titlepage():
     """Add title and version number of the specification to the titlepage."""
-    version_number, version_date = extract_header_string()
+    title, version_number, build_date = extract_header_string()
 
-    with open('./cover.tex', 'r') as file:
+    with open('cover.tex', 'r') as file:
         data = file.readlines()
 
     data[-1] = ("\\textsc{\large "+version_number+"}" +
                 "\\\\[0.5cm]" +
                 "{\large " +
-                version_date +
+                build_date +
                 "}" +
                 "\\\\[2cm]" +
                 "\\vfill" +
                 "\\end{titlepage}")
 
-    with open('./cover.tex', 'w') as file:
+    with open('cover.tex', 'w') as file:
         data = file.writelines(data)
 
 
 if __name__ == '__main__':
 
-    duplicated_src_dir_path = './src_copy'
+    duplicated_src_dir_path = 'src_copy/src'
 
     # Step 1: make a copy of the src directory in the current directory
     copy_src()
@@ -176,15 +173,19 @@ if __name__ == '__main__':
 
     # Step 3: copy images from subdirectories of src_copy directory
     copy_images(duplicated_src_dir_path)
+    subprocess.call("mv src_copy/src/images/images/* src_copy/src/images/", 
+                    shell=True)
 
-    # Step 4: extract the latest version number and date
+    # Step 4: extract the latest version number, date and title
     extract_header_string()
     add_header()
 
     edit_titlepage()
 
+    # Step 5: modify changelog to be a level 1 heading to facilitate section 
+    # separation
     modify_changelog()
 
-    # Step 5: remove all internal links
+    # Step 6: remove all internal links
     remove_internal_links(duplicated_src_dir_path, 'cross')
     remove_internal_links(duplicated_src_dir_path, 'same')
