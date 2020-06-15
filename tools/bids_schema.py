8d0b612bcce2e3a1a31d481e870d5b7a289a1a8b
Taylor Salo <tsalo006@fiu.edu>
2020-06-14 21:49:40 -0400

    Switch from click to argparse.
    This is primarily to make it easier to run things within Python, since
    click-decorated functions can’t be called like Python functions.

--- tools/bids_schema.py
+++ tools/bids_schema.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # pip install click click-didyoumean tabulate
-
+import argparse
 from itertools import chain
 import logging
 import os
@@ -9,21 +9,13 @@ from pathlib import Path
 import yaml
 import sys
 from warnings import warn
-
 import pandas as pd
-#
-import click
-from click_didyoumean import DYMGroup
-# from yamlinclude import YamlIncludeConstructor
-
-# helper to debug
 from pprint import pprint
+
 
 #
 # Aux utilities
 #
-
-
 def is_interactive():
     """Return True if all in/outs are tty"""
     # TODO: check on windows if hasattr check would work correctly and add value:
@@ -97,67 +89,62 @@ BIDS_SCHEMA = Path(__file__).parent.parent / "src" / "schema"
 #
 
 
-def print_version(ctx, param, value):
-    if not value or ctx.resilient_parsing:
-        return
-    click.echo(__version__)
-    ctx.exit()
-
-
 def upper(ctx, param, value):
     import pdb
 
     pdb.set_trace()
     return value.upper()
 
+
 #
 # Common options to reuse
 #
 # Functions to provide customizations where needed
-def _updated_option(*args, **kwargs):
-    args, d = args[:-1], args[-1]
-    kwargs.update(d)
-    return click.option(*args, **kwargs)
-
-
-def schema_path_option():
-    return click.option(
-        "--schema-path",
-        type=click.Path(exists=True, dir_okay=True),
-        default=str(BIDS_SCHEMA),
-        help="Path to the directory with the schema")
-
-
-# group to provide commands
-@click.group(cls=DYMGroup)
-@click.option(
-    "--version", is_flag=True, callback=print_version, expose_value=False, is_eager=True
-)
-@click.option(
-    "-l",
-    "--log-level",
-    help="Log level name",
-    # TODO: may be bring also handling of  int  values.  For now -- no need
-    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
-    default="INFO",
-    # callback=upper,  # TODO: not in effect! seems to come to play only after type validation
-    show_default=True,
-)
-@click.option("--pdb", help="Fall into pdb if errors out", is_flag=True)
-def main(log_level, pdb=False):
-    """A client to support BIDS schema manipulations.
-    """
-    set_logger_level(get_logger(), log_level)
-    if pdb:
-        #map_to_click_exceptions._do_map = False
-        setup_exceptionhook()
-
-
 def _get_entry_name(path):
     if path.suffix == '.yaml':
         return path.name[:-5]  # no .yaml
     else:
         return path.name
+
+
+def _get_parser():
+    """
+    Parses command line inputs for NiMARE
+
+    Returns
+    -------
+    parser.parse_args() : argparse dict
+    """
+    parser = argparse.ArgumentParser(prog='bids')
+    subparsers = parser.add_subparsers(help='BIDS workflows')
+    # show()
+    show_parser = subparsers.add_parser(
+        'show',
+        help=('Print out the schema'),
+    )
+    show_parser.set_defaults(func=show)
+    show_parser.add_argument(
+        'schema_path',
+        type=Path,
+        help=('Path to schema to show.')
+    )
+    # entity_table()
+    entity_parser = subparsers.add_parser(
+        'entity',
+        help=('Print entity table')
+    )
+    entity_parser.set_defaults(func=save_entity_table)
+    entity_parser.add_argument(
+        'schema_path',
+        type=Path,
+        help=('Path to schema to show.')
+    )
+    entity_parser.add_argument(
+        'out_file',
+        type=str,
+        help=('Output filename.')
+    )
+    return parser
 
 
 def load_schema(schema_path):
@@ -184,24 +171,18 @@ def load_schema(schema_path):
         warn(f"{schema_path} is somehow nothing we can load")
 
 
-@main.command()
-# TODO: may be add "format" and make it also json?
-@schema_path_option()
 def show(schema_path):
     """Print full schema."""
     schema = load_schema(schema_path)
     print(yaml.safe_dump(schema, default_flow_style=False))
 
 
-@main.command()
-# TODO: output path, for now just print
-@schema_path_option()
-def entity_table(schema_path, tablefmt="github"):
+def entity_table(schema_path):
     """Produce entity table (markdown) based on schema.
     This only works if the top-level schema *directory* is provided.
     """
-    from tabulate import tabulate
     schema = load_schema(schema_path)
+
     # prepare the table based on the schema
     # import pdb; pdb.set_trace()
     header = ['Entity', 'DataType']
@@ -256,6 +237,12 @@ def entity_table(schema_path, tablefmt="github"):
     table = table.set_index(('Entity', 'Format'))
 
     # Now we can split as needed
+    return table
+
+
+def github_entity_table(schema_path, tablefmt='github'):
+    from tabulate import tabulate
+    table = entity_table(schema_path)
 
     # Flatten multi-index
     vals = table.index.tolist()
@@ -266,10 +253,40 @@ def entity_table(schema_path, tablefmt="github"):
 
     # print it as markdown
     table = table.drop(columns=['DataType'])
-    header.remove('DataType')
-    print(tabulate(table, header, tablefmt=tablefmt))
-    return table
+    return tabulate(table, headers='keys', tablefmt=tablefmt)
+
+
+def save_entity_table(schema_path, out_file):
+
+    table = github_entity_table(schema_path)
+
+    intro_text = """\
+# Appendix IV: Entity table
+
+This section compiles the entities (key-value pairs) described throughout this
+specification, and establishes a common order within a filename. For example, if
+a file has an acquisition and reconstruction label, the acquisition entity must
+precede the reconstruction entity. REQUIRED and OPTIONAL entities for a given
+file type are denoted. Entity formats indicate whether the value is alphanumeric
+(`<label>`) or numeric (`<index>`).
+
+A general introduction to entities is given in the section on
+[file name structure](../02-common-principles.md#file-name-structure)
+"""
+    with open(out_file, 'w') as fo:
+        fo.write(intro_text)
+        fo.write('\n')
+        fo.write(table)
+        fo.write('\n')
+
+
+def _main(argv=None):
+    """BIDS schema CLI entrypoint"""
+    options = _get_parser().parse_args(argv)
+    args = vars(options).copy()
+    args.pop('func')
+    options.func(**args)
 
 
 if __name__ == '__main__':
-    main()
+    _main()
