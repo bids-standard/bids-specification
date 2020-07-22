bfac10207e2eb17473aa32450e5e5e5141e5169c
Taylor Salo <tsalo006@fiu.edu>
2020-07-22 11:04:13 -0400

    Remove top-level fieldmaps.

--- tools/bids_schema.py
+++ tools/bids_schema.py
@@ -322,8 +322,8 @@ This section compiles the entities (key-value pairs) described throughout this
 specification, and establishes a common order within a filename. 
 For example, if a file has an acquisition and reconstruction label, the
 acquisition entity must precede the reconstruction entity.
-REQUIRED and OPTIONAL entities for a given file type are denoted. Entity
-formats indicate whether the value is alphanumeric
+REQUIRED and OPTIONAL entities for a given file type are denoted.
+Entity formats indicate whether the value is alphanumeric
 (`<label>`) or numeric (`<index>`).
 
 A general introduction to entities is given in the section on
