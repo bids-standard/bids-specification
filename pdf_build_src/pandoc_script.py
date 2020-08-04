0ddd6095cf366adc5e1e50058793228aff3b0855
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-08-04 10:25:01 +0200

    Added functions to correct tables before pdf generation (Development still ongoing)

--- pdf_build_src/pandoc_script.py
+++ pdf_build_src/pandoc_script.py
@@ -5,7 +5,71 @@ This is done once the duplicate src directory is processed.
 import os
 import subprocess
 
+def correct_table(table):
+    """Compute the max number of characters and dashes for each column and create the corrected table"""
+    import numpy as np
+    new_table = []
+    nb_of_rows = len(table)
+    nb_of_cols = len(table[0])
+    nb_of_chars = []
+    for row in table:
+        nb_of_chars.append([len(elem) for elem in row])
+    nb_of_chars = np.array(nb_of_chars)
+    max_chars_in_cols = nb_of_chars.max(axis=0)
+    print('    - Number of chars in table cells: {}'.format(max_chars_in_cols))
 
+    for row in table:
+        for i, elem in enumerate(row):
+            if i == 0:
+                row_content = '|'
+            elif i > 0:
+                row_content += ' {:{align}{width}} |'.format(elem, align='<', width=(max_chars_in_cols[i]))
+
+        print(row_content)
+
+
+def correct_tables():
+    """Make sure that:
+    * proportion and number of dashes (---) are sufficiently enough for PDF generation, and 
+    * fences (|) are corrected aligned
+    """
+    markdown_list = []
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith(".md") and file != 'index.md' and file != '01-contributors.md':
+                print('Check tables in {}'.format(os.path.join(root, file)))
+                markdown_list.append(os.path.join(root, file))
+                with open(os.path.join(root, file),'r') as f:
+                    content = f.readlines()
+                line_nb = 0
+                tables = []
+                table_mode = False
+                for line in content:
+                    if line:
+                        # Use dashes to detect where a table start 
+                        if '--' in line and not table_mode:
+                            table_mode = True
+                            print('  * Detected table starting line {}'.format(line_nb-1))
+                            table = []
+                            header_row = [c.strip() for c in content[line_nb-1].split('|')]
+                            row = [c.strip() for c in line.split('|')]
+                            table.append(header_row)
+                            table.append(row)
+                        elif table_mode:
+                            row = [c.strip() for c in line.split('|')]
+                            # Add row to table if this is not the end of the table
+                            if row != ['']:
+                                table.append(row)
+                            else:
+                                table_mode = False
+                                table = correct_table(table)
+                                tables.append(table)
+
+
+                    line_nb += 1
+                #print(tables)
+
+           
 def build_pdf(filename):
     """Construct command with required pandoc flags and run using subprocess.
 
@@ -40,4 +104,5 @@ def build_pdf(filename):
 
 
 if __name__ == "__main__":
+    correct_tables()
     build_pdf('bids-spec.pdf')
