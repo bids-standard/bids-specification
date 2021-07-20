96131df3729cb8946c302141fcc0e0462bf4c4ad
Taylor Salo <tsalo006@fiu.edu>
2021-07-20 11:02:06 -0400

    [SCHEMA] Apply schema rules to entity values (#792)
    * Use `type` key from other parts of schema on entities.
    
    By using the same tools, we can also use things like `enum` to limit values.
    
    * Support the new approach in the tools.
    
    The current solution is a bit of a hack but it's easy to adjust. Basically, when `format` is missing from the schema, default to "label".
    
    * Update tools/schemacode/schema.py
    
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>
    
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>

--- tools/schemacode/schema.py
+++ tools/schemacode/schema.py
@@ -171,9 +171,14 @@ def make_entity_definitions(schema):
         text += "Full name: {}".format(entity_info["name"])
         text += "\n\n"
         text += "Format: `{}-<{}>`".format(
-            entity_info["entity"], entity_info["format"]
+            entity_info["entity"],
+            entity_info.get("format", "label"),
         )
         text += "\n\n"
+        if "enum" in entity_info.keys():
+            text += "Allowed values: `{}`".format("`, `".join(entity_info["enum"]))
+            text += "\n\n"
+
         text += "Definition: {}".format(entity_info["description"])
     return text
 
@@ -219,7 +224,7 @@ def make_filename_template(schema, **kwargs):
             for ent in entities:
                 ent_format = "{}-<{}>".format(
                     schema["entities"][ent]["entity"],
-                    schema["entities"][ent]["format"],
+                    schema["entities"][ent].get("format", "label")
                 )
                 if ent in group["entities"]:
                     if group["entities"][ent] == "required":
@@ -309,7 +314,7 @@ def make_entity_table(schema, tablefmt="github", **kwargs):
         entity_shorthand = schema["entities"][entity]["entity"]
         header.append(spec["name"])
         formats.append(
-            f'[`{entity_shorthand}-<{spec["format"]}>`]'
+            f'[`{entity_shorthand}-<{spec.get("format", "label")}>`]'
             f"({ENTITIES_FILE}#{entity_shorthand})"
         )
         entity_to_col[entity] = i + 1
