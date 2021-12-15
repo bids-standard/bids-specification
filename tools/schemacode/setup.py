1a7e713e6ba67555dcd051080bef67208d1bfc6d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2021-12-15 10:53:39 -0500

    RF: Use setup.cfg over info.py

--- tools/schemacode/setup.py
+++ tools/schemacode/setup.py
@@ -1,57 +1,18 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-""" schemacode setup script """
+import sys
+from setuptools import setup
+import versioneer
 
+# Give setuptools a hint to complain if it's too old a version
+# 30.3.0 allows us to put most metadata in setup.cfg
+# Should match pyproject.toml
+SETUP_REQUIRES = ['setuptools >= 30.3.0']
+# This enables setuptools to install wheel on-the-fly
+SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
-def main():
-    """ Install entry-point """
-    import os.path as op
-    from inspect import currentframe, getfile
-    from io import open
+if __name__ == '__main__':
+    setup(name="pybids",
+          version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass(),
+          setup_requires=SETUP_REQUIRES)
 
-    from setuptools import find_packages, setup
-
-    import versioneer
-
-    ver_file = op.join("schemacode", "info.py")
-    with open(ver_file) as f:
-        exec(f.read())
-    vars = locals()
-
-    root_dir = op.dirname(op.abspath(getfile(currentframe())))
-    cmdclass = versioneer.get_cmdclass()
-
-    pkg_data = {
-        "schemacode": [
-            "tests/data/*",
-        ]
-    }
-
-    setup(
-        name=vars["PACKAGENAME"],
-        version=vars["VERSION"],
-        description=vars["DESCRIPTION"],
-        long_description=vars["LONGDESC"],
-        long_description_content_type="text/markdown",
-        author=vars["AUTHOR"],
-        author_email=vars["EMAIL"],
-        maintainer=vars["MAINTAINER"],
-        maintainer_email=vars["EMAIL"],
-        url=vars["URL"],
-        license=vars["LICENSE"],
-        classifiers=vars["CLASSIFIERS"],
-        download_url=vars["DOWNLOAD_URL"],
-        # Dependencies handling
-        python_requires=vars["PYTHON_REQUIRES"],
-        install_requires=vars["REQUIRES"],
-        tests_require=vars["TESTS_REQUIRES"],
-        extras_require=vars["EXTRA_REQUIRES"],
-        entry_points=vars["ENTRY_POINTS"],
-        packages=find_packages(exclude=("tests",)),
-        zip_safe=False,
-        cmdclass=cmdclass,
-    )
-
-
-if __name__ == "__main__":
-    main()
