fa6256ce4eee084241007d69b71ea56c887e89fa
Taylor Salo <tsalo006@fiu.edu>
2022-01-05 13:25:36 -0500

    Add more valid pattern tests.

--- tools/schemacode/schemacode/tests/test_schema.py
+++ tools/schemacode/schemacode/tests/test_schema.py
@@ -37,13 +37,38 @@ def test_formats(schema_obj):
     GOOD_PATTERNS = {
         "label": ["01", "test", "test01", "Test01"],
         "index": ["01", "1", "10000", "00001"],
+        "string": ["any string is valid."],
+        "integer": ["5", "10", "-5", "-10"],
+        "number": ["5", "3.14", "-5", "-3.14"],
+        "boolean": ["true", "false"],
+        "date": ["2022-01-05", "2022-01-05UTC", "2022-50-50"],
+        "datetime": [
+            "2022-01-05T13:16:30",
+            "2022-01-05T13:16:30.05",
+            "2022-01-05T13:16:30UTC",
+            "2022-01-05T13:16:30.05UTC",
+        ],
+        "time": ["13:16:30"],
+        "unit": ["any string is valid."],
+        "stimuli_relative": ["any/arbitrary/path/file.txt"],
+        "dataset_relative": ["any/arbitrary/path/file.txt"],
+        "participant_relative": ["any/arbitrary/path/file.txt"],
+        "rrid": ["RRID:SCR_017398"],
+        "uri": ["foo://example.com:8042/over/there?name=ferret#nose"],
+        "bids_uri": [
+            "bids::sub-01/fmap/sub-01_dir-AP_epi.nii.gz",
+            "bids:ds000001:sub-02/anat/sub-02_T1w.nii.gz",
+            "bids:myderivatives:sub-03/func/sub-03_task-rest_space-MNI152_bold.nii.gz",
+        ],
     }
     for pattern, test_list in GOOD_PATTERNS.items():
         pattern_format = schema_obj["objects"]["formats"][pattern]["pattern"]
         search_pattern = "^" + pattern_format + "$"
         search = re.compile(search_pattern)
         for test_string in test_list:
-            assert bool(search.fullmatch(test_string))
+            assert bool(
+                search.fullmatch(test_string)
+            ), f"'{test_string}' is not a valid match for the pattern '{search.pattern}'"
 
     # Check that invalid strings do not match the search pattern.
     BAD_PATTERNS = {
