271964bb648bba4d0fa424c9d9f0e220df3b6cfe
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-04-17 00:05:33 -0400

    ENH: Add namespace object that allows attribute and key access to fields

--- tools/schemacode/schemacode/schema.py
+++ tools/schemacode/schemacode/schema.py
@@ -3,6 +3,7 @@ import logging
 import os
 from copy import deepcopy
 from pathlib import Path
+from collections.abc import Mapping
 
 import yaml
 
@@ -19,6 +20,97 @@ def _get_entry_name(path):
         return path.name[:-5]  # no .yaml
     else:
         return path.name
+
+
+def _expand_dots(entry):
+    # Helper function for expand
+    key, val = entry
+    if "." in key:
+        init, post = key.split(".", 1)
+        return init, dict([_expand_dots((post, val))])
+    return key, expand(val)
+
+
+def expand(element):
+    """ Expand a dict, recursively, to replace dots in keys with recursive dictionaries
+
+    Examples
+    --------
+    >>> expand({"a": 1, "b.c": 2, "d": [{"e": 3, "f.g": 4}]})
+    {'a': 1, 'b': {'c': 2}, 'd': [{'e': 3, 'f': {'g': 4}}]}
+    """
+    if isinstance(element, dict):
+        return {key: val for key, val in map(_expand_dots, element.items())}
+    elif isinstance(element, list):
+        return [expand(el) for el in element]
+    return element
+
+
+class Namespace(Mapping):
+    """ Provides recursive attribute style access to a dict-like structure
+
+    Examples
+    --------
+    >>> ns = Namespace.build({"a": 1, "b.c": "val"})
+    >>> ns.a
+    1
+    >>> ns["a"]
+    1
+    >>> ns.b
+    <Namespace {'c': 'val'}>
+    >>> ns["b"]
+    <Namespace {'c': 'val'}>
+    >>> ns.b.c
+    'val'
+    >>> ns["b.c"]
+    'val'
+    >>> ns["b"]["c"]
+    'val'
+    >>> ns.b["c"]
+    'val'
+    >>> ns["b"].c
+    'val'
+    """
+    def __init__(self, *args, **kwargs):
+        self._properties = dict(*args, **kwargs)
+
+    @classmethod
+    def build(cls, mapping):
+        """ Expand mapping recursively and return as namespace """
+        return cls(expand(mapping))
+
+    def __getattribute__(self, key):
+        # Return actual properties first
+        err = None
+        try:
+            return super().__getattribute__(key)
+        except AttributeError as e:
+            err = e
+
+        # Utilize __getitem__ but keep original error on failure
+        try:
+            return self[key]
+        except KeyError:
+            raise err
+
+    def __getitem__(self, key):
+        key, dot, subkey = key.partition(".")
+        val = self._properties[key]
+        if isinstance(val, dict):
+            val = self.__class__(val)
+        if dot:
+            # Recursive step
+            val = val[subkey]
+        return val
+
+    def __repr__(self):
+        return f"<Namespace {self._properties}>"
+
+    def __len__(self):
+        return len(self._properties)
+
+    def __iter__(self):
+        return iter(self._properties)
 
 
 def dereference_yaml(schema, struct):
