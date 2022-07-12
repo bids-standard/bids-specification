eeb322f481f72ed49cc753bae26a02927ccc2e70
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-07-12 11:37:54 -0400

    ENH: Load __version__ from SCHEMA_VERSION file

--- tools/schemacode/schemacode/__init__.py
+++ tools/schemacode/schemacode/__init__.py
@@ -1,8 +1,14 @@
 """A Python package for working with the BIDS schema."""
 from . import render, schema, utils
 
+from importlib import resources as ilr
+
 __all__ = [
     "render",
     "schema",
     "utils",
 ]
+
+version_file = ilr.files("schemacode.data") / "schema" / "SCHEMA_VERSION"
+with ilr.as_file(version_file) as path:
+    __version__ = path.read_text().strip()
