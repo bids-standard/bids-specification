96d474e5445f41a86bd2c5c139360e647b15308f
Horea Christian <chr@chymera.eu>
2022-08-18 18:09:38 -0400

    compressing archive

--- tools/schemacode/bidsschematools/tests/test_make_testdata.py
+++ tools/schemacode/bidsschematools/tests/test_make_testdata.py
@@ -1,5 +1,8 @@
 import os
+import shutil
+import tarfile
 
+from .. import version_file
 
 def test_make_archive(bids_examples, bids_error_examples, tmp_path):
     """
@@ -16,12 +19,20 @@ def test_make_archive(bids_examples, bids_error_examples, tmp_path):
         testdata archive creation is now inconspicuously posing as a test.
     """
 
-    import shutil
 
-    archive_path = os.path.join(tmp_path, "testdata")
+    bst_version = open(version_file).read().rstrip('\n')
+    archive_name = f"bidsschematools-testdata-{bst_version}"
+    data_path = os.path.join(tmp_path, archive_name)
+    archive_path = f"{data_path}.tar.gz"
+
     try:
-        os.mkdir(archive_path)
+        os.mkdir(data_path)
     except FileExistsError:
         pass
-    shutil.copytree(bids_examples, os.path.join(archive_path, "bids-examples"))
-    shutil.copytree(bids_error_examples, os.path.join(archive_path, "bids-error-examples"))
+    shutil.copytree(bids_examples, os.path.join(data_path, "bids-examples"))
+    shutil.copytree(bids_error_examples, os.path.join(data_path, "bids-error-examples"))
+
+    with tarfile.open(archive_path, "w:gz") as tar:
+        tar.add(data_path, arcname=os.path.basename(data_path))
+    print(archive_path)
+
