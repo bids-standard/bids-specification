1626747e65487c96e0cc3d6b74be4c8b54c7312c
Horea Christian <chr@chymera.eu>
2022-08-19 02:10:11 -0400

    black fix

--- tools/schemacode/bidsschematools/tests/test_make_testdata.py
+++ tools/schemacode/bidsschematools/tests/test_make_testdata.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import tarfile
 import tempfile
+
 import pytest
 
 from .. import version_file
@@ -11,10 +12,11 @@ def require_env(var):
     env = os.environ.get(var)
     return pytest.mark.skipif(
         not env,
-        reason=f"Release mode is not activated. Not generating static testdata archive. "
-		"In order to force testdata archive generation run: "
-		"`export BIDSSCHEMATOOLS_RELEASE_MODE_ACTIVATED=1`."
+        reason="Release mode is not activated. Not generating static testdata archive. "
+        "In order to force testdata archive generation run: "
+        "`export BIDSSCHEMATOOLS_RELEASE_MODE_ACTIVATED=1`.",
     )
+
 
 @require_env("BIDSSCHEMATOOLS_RELEASE_MODE_ACTIVATED")
 def test_make_archive(bids_examples, bids_error_examples):
