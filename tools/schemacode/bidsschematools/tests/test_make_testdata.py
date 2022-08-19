8bec8ec5c4c491e1f3b8a70f78f84522c2fc3ad0
Horea Christian <chr@chymera.eu>
2022-08-19 02:06:03 -0400

    Only keeping archive file and under an easily sourced path

--- tools/schemacode/bidsschematools/tests/test_make_testdata.py
+++ tools/schemacode/bidsschematools/tests/test_make_testdata.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import tarfile
+import tempfile
 import pytest
 
 from .. import version_file
@@ -16,7 +17,7 @@ def require_env(var):
     )
 
 @require_env("BIDSSCHEMATOOLS_RELEASE_MODE_ACTIVATED")
-def test_make_archive(bids_examples, bids_error_examples, tmp_path):
+def test_make_archive(bids_examples, bids_error_examples):
     """
     ATTENTION! This is not a test!
     Create static testdata archive containing the bidsschematools data reference whitelist.
@@ -29,19 +30,16 @@ def test_make_archive(bids_examples, bids_error_examples, tmp_path):
         (3) wrappers evaluating the yield statement
         (4) the desire to not download testdata twice for archive creation
         testdata archive creation is now inconspicuously posing as a test.
+    * Archives will be generated under `/tmp/bidsschematools-testdata-SCHEMA_VERSION.tar.gz`
     """
 
     bst_version = open(version_file).read().rstrip("\n")
     archive_name = f"bidsschematools-testdata-{bst_version}"
-    data_path = os.path.join(tmp_path, archive_name)
-    archive_path = f"{data_path}.tar.gz"
+    archive_path = f"/tmp/{archive_name}.tar.gz"
 
-    try:
-        os.mkdir(data_path)
-    except FileExistsError:
-        pass
-    shutil.copytree(bids_examples, os.path.join(data_path, "bids-examples"))
-    shutil.copytree(bids_error_examples, os.path.join(data_path, "bids-error-examples"))
+    with tempfile.TemporaryDirectory() as temp_path:
+        shutil.copytree(bids_examples, os.path.join(temp_path, "bids-examples"))
+        shutil.copytree(bids_error_examples, os.path.join(temp_path, "bids-error-examples"))
 
-    with tarfile.open(archive_path, "w:gz") as tar:
-        tar.add(data_path, arcname=os.path.basename(data_path))
+        with tarfile.open(archive_path, "w:gz") as tar:
+            tar.add(temp_path, arcname=archive_name)
