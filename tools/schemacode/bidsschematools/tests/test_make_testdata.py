9ca58c623b5f8f8d7c3800ee2d66263cfb72e29a
Horea Christian <chr@chymera.eu>
2022-08-19 17:50:24 -0400

    Generating testdata inside repo as opposed to in separate archive

--- tools/schemacode/bidsschematools/tests/test_make_testdata.py
+++ tools/schemacode/bidsschematools/tests/test_make_testdata.py
@@ -1,5 +1,6 @@
 import os
 import tarfile
+import shutil
 
 import pytest
 
@@ -30,9 +31,15 @@ def test_make_archive(bids_examples, bids_error_examples):
     * Archives will be generated under `/tmp/bidsschematools-testdata-SCHEMA_VERSION.tar.gz`
     """
 
-    archive_name = f"bidsschematools-testdata-{__version__}"
-    archive_path = f"/tmp/{archive_name}.tar.gz"
+    target_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"data","bundled"))
+    os.makedirs(target_dir, exist_ok = True)
+    shutil.copytree(bids_examples, f"{target_dir}/bids-examples")
+    shutil.copytree(bids_error_examples, f"{target_dir}/bids-error-examples")
 
-    with tarfile.open(archive_path, "w:gz") as tar:
-        tar.add(bids_examples, arcname=f"{archive_name}/bids-examples")
-        tar.add(bids_error_examples, arcname=f"{archive_name}/bids-error-examples")
+    # Keeping this for now, it would be really nice to have a separate archive someday.
+    # archive_name = f"bidsschematools-testdata-{__version__}"
+    # archive_path = f"/tmp/{archive_name}.tar.gz"
+
+    # with tarfile.open(archive_path, "w:gz") as tar:
+    #     tar.add(bids_examples, arcname=f"{archive_name}/bids-examples")
+    #     tar.add(bids_error_examples, arcname=f"{archive_name}/bids-error-examples")
