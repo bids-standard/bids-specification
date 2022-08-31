d19ac33687a3040f6d2d5a7b381ff1bc8146ca1f
Taylor Salo <tsalo006@fiu.edu>
2022-08-31 11:48:03 -0400

    Make element link info dependent on pdf build.

--- tools/schemacode/bidsschematools/render.py
+++ tools/schemacode/bidsschematools/render.py
@@ -478,9 +478,15 @@ def make_filename_template(
 
 def append_filename_template_legend(text, pdf_format=False):
     """Append a legend to filename templates."""
-    legend = """
+    if pdf_format:
+        info_str = ""
+    else:
+        info_str = """
 - For more information about filename elements (for example, entities, suffixes, extensions),
   follow the links embedded in the filename template.
+  """
+
+    legend = f"""{info_str}
 - Filename entities or folders between square brackets
   (for example, `[_ses-<label>]`) are OPTIONAL.
 - Some entities may only allow specific values,
