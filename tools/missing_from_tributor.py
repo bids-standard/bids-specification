387a13f422ef951c8c350ffca934a0663d064fef
Remi Gau <remi_gau@hotmail.com>
2022-09-22 11:31:13 +0200

    tributors update

--- tools/missing_from_tributor.py
+++ tools/missing_from_tributor.py
@@ -7,14 +7,23 @@ from utils import load_tributors, root_dir
 tributors_file = root_dir().joinpath(".tributors")
 
 
-tsv = pd.read_csv("bids_contributors_old.tsv", sep="\t", encoding="utf8")
-
 tributors = load_tributors(tributors_file)
 tributors_names = [tributors[x]["name"].rstrip() for x in tributors]
 
-print("\n[red]NOT IN .tributors FILE[/red]")
+tsv = pd.read_csv("bids_contributors_old.tsv", sep="\t", encoding="utf8")
+
+print("\n[red]From old contributors and NOT IN .tributors FILE[/red]")
 in_tributors = tsv.name.isin(tributors_names).to_list()
 for i, value in enumerate(in_tributors):
     if not value:
         print(tsv.name[i])
+print("\n")
+
+tsv = pd.read_csv("BIDS-contributor_emails.tsv", sep="\t", encoding="utf8")
+
+print("\n[red]From email list and NOT IN .tributors FILE[/red]")
+in_tributors = tsv.Name.isin(tributors_names).to_list()
+for i, value in enumerate(in_tributors):
+    if not value:
+        print(tsv.Name[i])
 print("\n")
