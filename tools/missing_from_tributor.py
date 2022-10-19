8ba0de2c36ac75ae84b98ea106a38e5d93dc10e9
Remi Gau <remi_gau@hotmail.com>
2022-10-19 17:08:41 +0200

    add from old mailing list

--- tools/missing_from_tributor.py
+++ tools/missing_from_tributor.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from rich import print
 
-from utils import load_tributors, root_dir
+from utils import load_tributors, root_dir, add_to_tributors, add_to_allcontrib
 
 
 tributors_file = root_dir().joinpath(".tributors")
+allcontrib_file = root_dir().joinpath(".all-contributorsrc")
 
 tributors = load_tributors(tributors_file)
 tributors_names = [tributors[x]["name"].rstrip() for x in tributors]
@@ -18,9 +19,6 @@ in_tributors = bids_contributors_old.Name.isin(tributors_names).to_list()
 for i, value in enumerate(in_tributors):
     if not value:
         print(f"{bids_contributors_old.Name[i]} {bids_contributors_old.Email[i]}")
+        add_to_tributors(tributors_file, bids_contributors_old.Name[i])
+        add_to_allcontrib(allcontrib_file, bids_contributors_old.Name[i])
 print("\n")
-
-bids_contributors_old = pd.read_csv(
-    "bids_contributors_old.tsv", sep="\t", encoding="utf8"
-)
-bids_contributors_old_emails_names = [x.strip() for x in bids_contributors_old.name]
