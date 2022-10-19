481bd58551714217cdf190071939f6741a521bc3
Remi Gau <remi_gau@hotmail.com>
2022-10-19 18:03:57 +0200

    add header to contributor table

--- tools/print_contributors.py
+++ tools/print_contributors.py
@@ -9,13 +9,22 @@ tmp_file = Path(__file__).parent.joinpath("tmp.md")
 
 
 def contributor_table_header(max_name_length, max_contrib_length):
-    return f"""
+    return f"""# Contributors
+
+Legend (source:
+<https://allcontributors.org/docs/en/emoji-key>)
+
+The following individuals have contributed to the Brain Imaging Data Structure
+ecosystem (in alphabetical order).
+If you contributed to the BIDS ecosystem and your name is not listed, please add it.
+
 |name{" " * (max_name_length-4)}|contributions{" " * (max_contrib_length-13)}|
 |{"-" * max_name_length}|{"-"*max_contrib_length}|
 """
 
 
 def create_line_contributor(contributor, max_name_length, max_contrib_length):
+    # TODO add link to contributor blog? or orcid?
     name = contributor["name"]
     line = f"| {name}{' '*(max_name_length-len(name)-1)}|"
     nb_contrib = len(contributor["contributions"]) * 2
