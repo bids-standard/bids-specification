2eab2aa4555b9d38d47197ed862314651a17b89a
Remi Gau <remi_gau@hotmail.com>
2022-10-19 17:04:56 +0200

    delete dead code

--- tools/missing_from_tributor.py
+++ tools/missing_from_tributor.py
@@ -4,22 +4,6 @@ from rich import print
 from utils import load_tributors, root_dir
 
 
-def rename_columns(df):
-    df.rename(
-        columns={
-            "Name  For example: Jane Smith": "name",
-            "Primary affiliation  Please include:  Institution, Department, City, (State), Zip Code, Country": "affiliation",
-            "ORCID number  For example:  https://orcid.org/0000-0002-1535-9777": "orcid",
-            "Github username  For example:   https://github.com/Jane-Smith": "github",
-            "I agree to have my email address on the github repository of the BIDS specification": "consent",
-            "Email address": "email",
-            "Added to .tributors file in repo": "added",
-        },
-        inplace=True,
-    )
-    return df
-
-
 tributors_file = root_dir().joinpath(".tributors")
 
 tributors = load_tributors(tributors_file)
@@ -33,24 +17,10 @@ print("\n[red]From email list and NOT IN .tributors FILE[/red]")
 in_tributors = bids_contributors_old.Name.isin(tributors_names).to_list()
 for i, value in enumerate(in_tributors):
     if not value:
-        print(bids_contributors_old.Name[i])
+        print(f"{bids_contributors_old.Name[i]} {bids_contributors_old.Email[i]}")
 print("\n")
 
 bids_contributors_old = pd.read_csv(
     "bids_contributors_old.tsv", sep="\t", encoding="utf8"
 )
 bids_contributors_old_emails_names = [x.strip() for x in bids_contributors_old.name]
-
-bids_contributors_new_emails = pd.read_csv(
-    "contributors.tsv", sep="\t", encoding="utf8"
-)
-bids_contributors_new_emails = rename_columns(bids_contributors_new_emails)
-bids_contributors_new_emails_names = [
-    x.strip() for x in bids_contributors_new_emails.name
-]
-
-print("\n[red]From new email list and not in old email list[/red]")
-new_ones = set(bids_contributors_new_emails_names) - set(
-    bids_contributors_old_emails_names
-)
-print(new_ones)
