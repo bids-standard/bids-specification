9de343adf1108a1985e2577a33f28da845b880d0
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2022-10-27 15:38:27 +0200

    Apply suggestions from code review
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>

--- pdf_build_src/pandoc_script.py
+++ pdf_build_src/pandoc_script.py
@@ -18,55 +18,29 @@ def build_pdf(filename="bids-spec.pdf", logfile="bids-spec_pandoc_log.json"):
     logfile : str
         Name of the log file. Defaults to "bids-spec_pandoc_log.json".
     """
-    # change directory name if running on CI
-    runs_on_CI = True if os.getenv("CIRCLE_BRANCH") else False
-    proj_fname = "bids-specification"
-    if runs_on_CI:
-        proj_fname = "project"
 
-    # location of this file: This is also the working directory when
-    # the pdf is being built using `cd build_pdf_src` and then
-    # `bash build_pdf.sh`
-    root = pathlib.Path(__file__).parent.absolute()
+    def _flatten_values(lst):
+        """Flatten a list of dicts of lists to a list of values."""
+        for val in (obj.values() for obj in lst):
+            if isinstance(val, str):
+                yield val
+            else:
+                yield from _flatten_values(val)
 
-    # Parse content files from mkdocs.yml file
-    def _unpack(sections):
-        """Flatten a list of dicts of lists."""
-        sections_flat = []
-        for i in sections:
-            if isinstance(list(i.values())[0], str):
-                sections_flat.append(i)
-            elif isinstance(list(i.values())[0], list):
-                for j in list(i.values())[0]:
-                    sections_flat.append(j)
-        return sections_flat
-
-    def _has_only_str2str(sections):
-        """Check list of dict has only str to str mappings."""
-        for i in sections:
-            if not isinstance(list(i.values())[0], str):
-                return False
-        return True
-
-    fname_mkdocs_yml = [
-        i for i in list(root.parents) if str(i).endswith(proj_fname)
-    ][0] / "mkdocs.yml"
+      # Find mkdocs.yml in some parent directory of this script
+      fname_mkdocs_yml = next(
+          path / "mkdocs.yml"
+          for path in pathlib.Path(__file__).absolute().parents
+          if Path.isfile(path / "mkdocs.yml")
+      )
     with open(fname_mkdocs_yml, "r") as stream:
         mkdocs_yml = yaml.safe_load(stream)
 
     sections = mkdocs_yml["nav"][0]["The BIDS Specification"]
-    while not _has_only_str2str(sections):
-        sections = _unpack(sections)
-
-    markdown_list = [list(i.values())[0] for i in sections]
 
     # special files
     index_page = "./index.md"
-    pandoc_metadata = (
-        [i for i in list(root.parents) if str(i).endswith(proj_fname)][0]
-        / "pdf_build_src"
-        / "metadata.yml"
-    )
+    pandoc_metadata = pathlib.Path(__file__).with_name("metadata.yml")
 
     # Prepare the command options
     cmd = [
@@ -92,7 +66,7 @@ def build_pdf(filename="bids-spec.pdf", logfile="bids-spec_pandoc_log.json"):
     # The filenames in `markdown_list` will ensure correct order when sorted
     cmd += [str(root / index_page)]
     cmd += [str(pandoc_metadata)]
-    cmd += [str(root / i) for i in markdown_list]
+    cmd += [str(root / md) for md in _flatten_values(sections)]
 
     # print and run
     print("pandoc command being run: \n\n" + "\n".join(cmd))
