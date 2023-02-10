0a7e60830ef3eef54ebe058e7935514d4b353971
Horea Christian <chr@chymera.eu>
2023-02-10 14:10:09 -0500

    Added `bst validate`

--- tools/schemacode/bidsschematools/__main__.py
+++ tools/schemacode/bidsschematools/__main__.py
@@ -1,7 +1,9 @@
 import argh
 
 from .schema import export
+from .validator import validate_bids
 
 
 def cli():
-    argh.dispatch_commands([export])
+    validate_bids.__name__ = 'validate'
+    argh.dispatch_commands([export, validate_bids])
