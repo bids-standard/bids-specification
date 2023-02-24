cb84c7c661f7b97301f1ef493fb6ee624f7e0244
Chris Markiewicz <effigies@gmail.com>
2023-02-24 17:26:21 -0500

    TEST: Test all selectors and checks, expect parse errors

--- tools/schemacode/bidsschematools/tests/test_expressions.py
+++ tools/schemacode/bidsschematools/tests/test_expressions.py
@@ -1,6 +1,7 @@
 import pytest
+from pyparsing.exceptions import ParseException
 
-from ..expressions import expression
+from ..expressions import ASTNode, expression
 
 
 def test_schema_expressions(schema_obj):
@@ -34,3 +35,45 @@ def test_expression_associations(expr, disambiguated):
     assert len(parse_results) == 1
     ast = parse_results[0]
     assert str(ast) == disambiguated
+
+
+def test_selectors(schema_obj):
+    # Selectors happen at the second level in a file
+    # For directories of files, need to look three down
+    for rules, level in (
+        (schema_obj.rules.checks, 3),
+        (schema_obj.rules.dataset_metadata, 2),
+        (schema_obj.rules.sidecars, 3),
+        (schema_obj.rules.tabular_data, 3),
+        (schema_obj.rules.errors, 2),
+    ):
+        keys = (key for key in rules.keys(level=level) if key.endswith("selectors"))
+        for key in keys:
+            for selector in rules[key]:
+                ast = expression.parse_string(selector)[0]
+                assert isinstance(ast, ASTNode)
+
+
+def test_checks(schema_obj):
+    checks = (
+        expr
+        for key, value in schema_obj.rules.checks.items(level=3)
+        if key.endswith("checks")
+        for expr in value
+    )
+    for check in checks:
+        ast = expression.parse_string(check)[0]
+        assert isinstance(ast, ASTNode)
+
+
+@pytest.mark.parametrize(
+    "expr",
+    (
+        "func(x, y",
+        "lhs == ",
+        "assignment = not.a.thing",
+    ),
+)
+def test_expected_failures(expr):
+    with pytest.raises(ParseException):
+        expression.parse_string(expr)
