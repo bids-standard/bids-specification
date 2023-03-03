907598db475985f2861f159a4b9938825d66e529
Remi Gau <remi_gau@hotmail.com>
2023-03-03 19:42:43 +0100

    refactor

--- tools/add_contributors.py
+++ tools/add_contributors.py
@@ -40,80 +40,8 @@ TOKEN_FILE = None
 INPUT_FILE = Path(__file__).parent / "new_contributors.tsv"
 
 
-def updating_allcontrib(allcontrib: dict, this_contributor: dict[str, str]) -> dict:
-    allcontrib_names = [x["name"] for x in allcontrib["contributors"]]
-
-    if this_contributor["name"] not in allcontrib_names:
-        print(f"adding {this_contributor['name']}")
-        allcontrib["contributors"].append(this_contributor)
-        return allcontrib
-
-    else:
-        index_allcontrib = allcontrib_names.index(this_contributor["name"])
-
-        for key, value in this_contributor.items():
-            if key not in allcontrib["contributors"][index_allcontrib]:
-                update_allcontrib_key(
-                    contributor=allcontrib["contributors"][index_allcontrib],
-                    key=key,
-                    value=value,
-                )
-
-            if allcontrib["contributors"][index_allcontrib][key] != value:
-                update_allcontrib_key(
-                    contributor=allcontrib["contributors"][index_allcontrib],
-                    key=key,
-                    value=value,
-                )
-
-    return allcontrib
-
-
-def update_allcontrib_key(contributor: dict, key: str, value: str):
-    print(f"updating {contributor['name']} - {key}")
-    contributor[key] = value
-
-
 def root_dir():
     return Path(__file__).parent.parent
-
-
-def load_tributors(tributors_file: Path):
-    """Load .tributors file."""
-    with open(tributors_file, "r", encoding="utf8") as tributors_file:
-        return json.load(tributors_file)
-
-
-def write_tributors(tributors_file: Path, tributors):
-    """Write .tributors file."""
-    tributors = sort_tributors(tributors)
-    with open(tributors_file, "w", encoding="utf8") as output_file:
-        json.dump(tributors, output_file, indent=4, ensure_ascii=False)
-
-
-def load_allcontrib(allcontrib_file: Path):
-    """Load .all-contributorsrc file."""
-    with open(allcontrib_file, "r", encoding="utf8") as input_file:
-        return json.load(input_file)
-
-
-def write_allcontrib(allcontrib_file: Path, allcontrib):
-    """Write .all-contributorsrc file."""
-    allcontrib = sort_all_contrib(allcontrib)
-    with open(allcontrib_file, "w", encoding="utf8") as output_file:
-        json.dump(allcontrib, output_file, indent=4, ensure_ascii=False)
-
-
-def load_citation(citation_file: Path):
-    """Load CITATION.CFF file."""
-    with open(citation_file, "r", encoding="utf8") as input_file:
-        return yaml.load(input_file)
-
-
-def write_citation(citation_file: Path, citation):
-    """Write CITATION.CFF file."""
-    with open(citation_file, "w", encoding="utf8") as output_file:
-        return yaml.dump(citation, output_file)
 
 
 def emoji_map() -> dict[str, str]:
@@ -146,39 +74,74 @@ def emoji_map() -> dict[str, str]:
     }
 
 
-def transfer_contribution(tributors: dict, allcontrib: dict) -> dict:
-    """Transfer contribution list from tributors to allcontrib."""
+def return_this_contributor(df: pd.DataFrame, name: str):
+    name = name.strip()
 
-    tributors_keys = list(tributors.keys())
-    tributors_names = [tributors[x]["name"].rstrip() for x in tributors]
+    mask = df.name == name
 
-    allcontrib_names = [x["name"] for x in allcontrib["contributors"]]
+    github = df[mask].github.values[0]
+    if pd.isna(github) or not isinstance(github, (str)):
+        github = None
 
-    for name in tributors_names:
-        index_allcontrib = allcontrib_names.index(name)
+    github_username = None
+    if github is not None:
+        github_username = github.replace("https://github.com/", "").strip(" ")
+    if github_username is None:
+        github_username = name.lower().replace(" ", "_")
 
-        index_tributor = tributors_names.index(name)
-        key_tributor = tributors_keys[index_tributor]
+    contributions = df[mask].contributions[0]
+    if pd.isna(contributions) or contributions is None:
+        raise ValueError(f"Contributions for {name} not defined in input file.")
+    allowed_contributions = list(emoji_map().keys())[0]
+    if any(x for x in contributions if x not in allowed_contributions):
+        raise ValueError(
+            f"Contributions must be one of {allowed_contributions}."
+            f" Got '{contributions}' for {name}."
+        )
 
-        contributions = tributors[key_tributor].get("contributions", ["doc"])
-        allcontrib["contributors"][index_allcontrib]["contributions"] = contributions
+    website = df[mask].website.values[0]
+    if pd.isna(website) or not isinstance(website, (str)):
+        website = github
 
-    return allcontrib
+    affiliation = df[mask].affiliation.values[0]
+    if pd.isna(affiliation) or not isinstance(affiliation, (str)):
+        affiliation = None
+
+    orcid = df[mask].orcid.values[0]
+    if pd.isna(orcid) or not isinstance(orcid, (str)):
+        orcid = None
+    if orcid is not None:
+        orcid = orcid.replace("http://", "https://")
+
+    email = df[mask].email.values[0]
+    if pd.isna(email) or not isinstance(email, (str)):
+        email = None
+
+    return {
+        "name": name,
+        "github": github,
+        "github_username": github_username,
+        "blog": website,
+        "affiliation": affiliation,
+        "orcid": orcid,
+        "email": email,
+    }
 
 
-def get_gh_avatar(gh_username: str, auth_username: str, auth_token: str):
-    """Return url of github avatar."""
+"""TRIBUTORS"""
 
-    avatar_url = None
 
-    if gh_username is not None:
-        print(f"getting avatar: {gh_username}")
-        url = f"https://api.github.com/users/{gh_username}"
-        response = requests.get(url, auth=(auth_username, auth_token))
-        if response.status_code == 200:
-            avatar_url = response.json()["avatar_url"]
+def load_tributors(tributors_file: Path):
+    """Load .tributors file."""
+    with open(tributors_file, "r", encoding="utf8") as tributors_file:
+        return json.load(tributors_file)
 
-    return avatar_url
+
+def write_tributors(tributors_file: Path, tributors):
+    """Write .tributors file."""
+    tributors = sort_tributors(tributors)
+    with open(tributors_file, "w", encoding="utf8") as output_file:
+        json.dump(tributors, output_file, indent=4, ensure_ascii=False)
 
 
 def return_missing_from_tributors(tributors_file: Path, names: list[str]) -> list[str]:
@@ -212,6 +175,22 @@ def add_to_tributors(tributors, this_contributor: str):
     return tributors
 
 
+"""ALCONTRIB"""
+
+
+def load_allcontrib(allcontrib_file: Path):
+    """Load .all-contributorsrc file."""
+    with open(allcontrib_file, "r", encoding="utf8") as input_file:
+        return json.load(input_file)
+
+
+def write_allcontrib(allcontrib_file: Path, allcontrib):
+    """Write .all-contributorsrc file."""
+    allcontrib = sort_all_contrib(allcontrib)
+    with open(allcontrib_file, "w", encoding="utf8") as output_file:
+        json.dump(allcontrib, output_file, indent=4, ensure_ascii=False)
+
+
 def sort_all_contrib(allcontrib):
     for i, contrib in enumerate(allcontrib["contributors"]):
         allcontrib["contributors"][i] = dict(OrderedDict(sorted(contrib.items())))
@@ -221,23 +200,86 @@ def sort_all_contrib(allcontrib):
     return allcontrib
 
 
-def add_to_allcontrib(allcontrib_file, user: str):
-    user = user.strip()
-    allcontrib = load_allcontrib(allcontrib_file)
+def updating_allcontrib(allcontrib: dict, this_contributor: dict[str, str]) -> dict:
     allcontrib_names = [x["name"] for x in allcontrib["contributors"]]
-    if user not in allcontrib_names:
-        print(f"adding {user} to {allcontrib_file}")
-        user_login = user.lower().replace(" ", "_")
-        allcontrib["contributors"].append(
-            {
-                "name": user,
-                "login": user_login,
-                "contributions": [
-                    "doc",
-                ],
-            }
-        )
-        write_allcontrib(allcontrib_file, allcontrib)
+
+    if this_contributor["name"] not in allcontrib_names:
+        print(f"adding {this_contributor['name']}")
+        allcontrib["contributors"].append(this_contributor)
+        return allcontrib
+
+    else:
+        index_allcontrib = allcontrib_names.index(this_contributor["name"])
+
+        for key, value in this_contributor.items():
+            if key not in allcontrib["contributors"][index_allcontrib]:
+                update_allcontrib_key(
+                    contributor=allcontrib["contributors"][index_allcontrib],
+                    key=key,
+                    value=value,
+                )
+
+            if allcontrib["contributors"][index_allcontrib][key] != value:
+                update_allcontrib_key(
+                    contributor=allcontrib["contributors"][index_allcontrib],
+                    key=key,
+                    value=value,
+                )
+
+    return allcontrib
+
+
+def get_gh_avatar(gh_username: str, auth_username: str, auth_token: str):
+    """Return url of github avatar."""
+
+    avatar_url = None
+
+    if gh_username is not None:
+        print(f"getting avatar: {gh_username}")
+        url = f"https://api.github.com/users/{gh_username}"
+        response = requests.get(url, auth=(auth_username, auth_token))
+        if response.status_code == 200:
+            avatar_url = response.json()["avatar_url"]
+
+    return avatar_url
+
+
+def update_allcontrib_key(contributor: dict, key: str, value: str):
+    print(f"updating {contributor['name']} - {key}")
+    contributor[key] = value
+
+
+def rename_keys_for_allcontrib(this_contributor):
+    renaming_map = {
+        "name": "name",
+        "avatar_url": "avatar_url",
+        "github_username": "login",
+        "login": "login",
+        "blog": "profile",
+        "contributions": "contributions",
+    }
+
+    renamed = {
+        renaming_map[key]: this_contributor[key]
+        for key in this_contributor
+        if key in renaming_map
+    }
+    return renamed
+
+
+"""CITATION.CFF"""
+
+
+def load_citation(citation_file: Path):
+    """Load CITATION.CFF file."""
+    with open(citation_file, "r", encoding="utf8") as input_file:
+        return yaml.load(input_file)
+
+
+def write_citation(citation_file: Path, citation):
+    """Write CITATION.CFF file."""
+    with open(citation_file, "w", encoding="utf8") as output_file:
+        return yaml.dump(citation, output_file)
 
 
 def return_author_list_for_cff(tributors_file):
@@ -282,76 +324,7 @@ def return_author_list_for_cff(tributors_file):
     return author_list
 
 
-def return_this_contributor(tsv: pd.DataFrame, name: str):
-    name = name.strip()
-
-    mask = tsv.name == name
-
-    github = tsv[mask].github.values[0]
-    if pd.isna(github) or not isinstance(github, (str)):
-        github = None
-
-    github_username = None
-    if github is not None:
-        github_username = github.replace("https://github.com/", "").strip(" ")
-    if github_username is None:
-        github_username = name.lower().replace(" ", "_")
-
-    contributions = tsv[mask].contributions[0]
-    if pd.isna(contributions) or contributions is None:
-        raise ValueError(f"Contributions for {name} not defined in input file.")
-    allowed_contributions = list(emoji_map().keys())[0]
-    if any(x for x in contributions if x not in allowed_contributions):
-        raise ValueError(
-            f"Contributions must be one of {allowed_contributions}."
-            f" Got '{contributions}' for {name}."
-        )
-
-    website = tsv[mask].website.values[0]
-    if pd.isna(website) or not isinstance(website, (str)):
-        website = github
-
-    affiliation = tsv[mask].affiliation.values[0]
-    if pd.isna(affiliation) or not isinstance(affiliation, (str)):
-        affiliation = None
-
-    orcid = tsv[mask].orcid.values[0]
-    if pd.isna(orcid) or not isinstance(orcid, (str)):
-        orcid = None
-    if orcid is not None:
-        orcid = orcid.replace("http://", "https://")
-
-    email = tsv[mask].email.values[0]
-    if pd.isna(email) or not isinstance(email, (str)):
-        email = None
-
-    return {
-        "name": name,
-        "github": github,
-        "github_username": github_username,
-        "blog": website,
-        "affiliation": affiliation,
-        "orcid": orcid,
-        "email": email,
-    }
-
-
-def rename_keys_for_allcontrib(this_contributor):
-    renaming_map = {
-        "name": "name",
-        "avatar_url": "avatar_url",
-        "github_username": "login",
-        "login": "login",
-        "blog": "profile",
-        "contributions": "contributions",
-    }
-
-    renamed = {
-        renaming_map[key]: this_contributor[key]
-        for key in this_contributor
-        if key in renaming_map
-    }
-    return renamed
+"""MAIN"""
 
 
 def main():
@@ -367,12 +340,13 @@ def main():
     allcontrib_file = root_dir() / ".all-contributorsrc"
     citation_file = root_dir() / "CITATION.cff"
 
-    print("\n[red]NOT IN .tributors FILE[/red]")
     new_contrib_names = df.name.to_list()
     missing_from_tributors = return_missing_from_tributors(
         tributors_file, new_contrib_names
     )
-    print(missing_from_tributors)
+    if len(missing_from_tributors) != 0:
+        print("\n[red]NOT IN .tributors FILE[/red]")
+        print(missing_from_tributors)
 
     tributors = load_tributors(tributors_file)
     allcontrib = load_allcontrib(allcontrib_file)
