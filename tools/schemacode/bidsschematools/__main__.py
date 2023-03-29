5c30f7aa3c3528d034e44aabd4c4f2fa15da49c6
Horea Christian <chr@chymera.eu>
2023-03-29 16:55:29 -0400

    CLI fixes
    Co-authored-by: Christopher J. Markiewicz <effigies@gmail.com>

--- tools/schemacode/bidsschematools/__main__.py
+++ tools/schemacode/bidsschematools/__main__.py
@@ -11,11 +11,10 @@ def cli():
     parser = argparse.ArgumentParser(
         description="BIDS Schema Tools Interface",
         formatter_class=argparse.RawTextHelpFormatter,
-        # add_help=False,
     )
-    parser.add_argument("-v", "--verbose", default=0, type=int)
+    parser.add_argument("--verbose", "-v", action="count", default=0)
     validate_bids.__name__ = "validate"
     argh.add_commands(parser, [export, validate_bids])
     args = parser.parse_args()
-    logging.getLogger("bidsschematools").setLevel(logging.INFO - args.verbose * 10)
+    logging.getLogger("bidsschematools").setLevel(logging.WARNING - args.verbose * 10)
     argh.dispatch(parser)
