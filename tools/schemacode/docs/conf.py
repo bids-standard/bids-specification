1e754682e837a6d5b331a5916423b015b708996f
Chris Markiewicz <effigies@gmail.com>
2023-05-22 21:35:13 -0400

    DOC: Add light and dark logos

--- tools/schemacode/docs/conf.py
+++ tools/schemacode/docs/conf.py
@@ -56,4 +56,9 @@ html_theme = "furo"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ["_static"]
+
+html_theme_options = {
+    "light_logo": "black_logo.svg",
+    "dark_logo": "white_logo.svg",
+}
