f7d2af053cdeec12758f6bc9777b2e1c181deb2c
Chris Markiewicz <effigies@gmail.com>
2024-02-08 15:52:48 -0500

    ENH: Create a pre-receive hook that accepts ignore rules and file listing

--- tools/schemacode/bidsschematools/__main__.py
+++ tools/schemacode/bidsschematools/__main__.py
@@ -1,6 +1,8 @@
 import logging
 import os
+import re
 import sys
+from itertools import chain
 
 import click
 
@@ -9,8 +11,9 @@ if sys.version_info < (3, 9):
 else:
     from importlib.resources import files
 
-
+from .rules import regexify_filename_rules
 from .schema import export_schema, load_schema
+from .validator import _bidsignore_check
 
 
 @click.group()
@@ -51,6 +54,63 @@ def export_metaschema(ctx, output):
         output = os.path.abspath(output)
         with open(output, "w") as fobj:
             fobj.write(metaschema)
+
+
+@cli.command("pre-receive-hook")
+@click.option("--schema", "-s", type=click.Path(), help="Path to the BIDS schema")
+@click.option(
+    "--input", "-i", "input_", default="-", type=click.Path(), help="Input file (default: stdin)"
+)
+@click.option(
+    "--output",
+    "-o",
+    "output",
+    default="-",
+    type=click.Path(),
+    help="Output file (default: stdout)",
+)
+def pre_receive_hook(schema, input_, output):
+    """Validate filenames from a list of files against the BIDS schema
+
+    The input should be a list of ignore patterns followed by a line containing
+    "0001" and then a list of filenames. The output will be a list of filenames
+    that do not match the schema.
+
+    This is intended to be used in a git pre-receive hook.
+    """
+    # Slurp inputs for now; we can think about streaming later
+    if input_ == "-":
+        lines = sys.stdin.readlines()
+    else:
+        with open(input_) as fobj:
+            lines = fobj.readlines()
+
+    split = lines.index("0001\n")
+    ignore = [line.rstrip() for line in lines[:split]]
+    filenames = [line.rstrip() for line in lines[split + 1 :]]
+
+    schema = load_schema(schema)
+    all_rules = chain.from_iterable(
+        regexify_filename_rules(group, schema, level=2)
+        for group in (schema.rules.files.common, schema.rules.files.raw)
+    )
+    regexes = [rule["regex"] for rule in all_rules]
+    # XXX Hack for phenotype files - this can be removed once we
+    # have a schema definition for them
+    regexes.append(r"phenotype/.*\.tsv")
+
+    output = sys.stdout if output == "-" else open(output, "w")
+
+    rc = 0
+    with output:
+        for filename in filenames:
+            if any(_bidsignore_check(pattern, filename, "") for pattern in ignore):
+                continue
+            if not any(re.match(regex, filename) for regex in regexes):
+                output.write(f"{filename}\n")
+                rc = 1
+
+    sys.exit(rc)
 
 
 if __name__ == "__main__":
