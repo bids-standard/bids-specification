3ddf673c376fb49eb22f23fba65c423e61be0726
Ben Dichter <ben.dichter@gmail.com>
2024-04-16 12:01:27 -0500

    add tests

---
+++ src/schema_validation/test_validate_metaschema.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import pytest
+from jsonschema.exceptions import ValidationError
+
+from .validate_schema import load_schema, derefence_schema, validate_schema
+
+schema_path = Path(__file__).resolve().parent.parent / "schema"
+
+
+def test_load_schema():
+    """Test that a valid schema does not raise an error."""
+    load_schema(schema_path)
+
+
+def test_derefence_schema():
+    """Test that a valid schema does not raise an error."""
+    schema = load_schema(schema_path)
+    derefence_schema(schema)
+
+
+def test_valid_schema():
+    """Test that a valid schema does not raise an error."""
+    schema = load_schema(schema_path)
+    validate_schema(schema)
+
+
+def test_broken_reference():
+    """Test that a broken reference raises an error."""
+    schema = load_schema(schema_path)
+    schema["rules"]["files"]["deriv"]["preprocessed_data"]["anat_nonparametric_common"]["$ref"] = "broken.reference"
+    with pytest.raises(KeyError) as e:
+        derefence_schema(schema)
+
+    assert "'broken'" in str(e.value)
+
+
+def test_add_legal_field():
+    """Test that adding a legal field does not raise an error."""
+    schema = load_schema(schema_path)
+    schema["rules"]["files"]["deriv"]["preprocessed_data"]["anat_nonparametric_common"]["entities"]["density"] = "optional"
+    validate_schema(schema)
+
+
+def test_invalid_value():
+    """Test that an invalid value raises an error."""
+    schema = load_schema(schema_path)
+    schema["rules"]["files"]["deriv"]["preprocessed_data"]["anat_nonparametric_common"]["entities"]["density"] = "invalid"
+    with pytest.raises(ValidationError) as e:
+        validate_schema(schema)
+
+    assert "invalid" in str(e.value)
+
---
+++ src/schema_validation/validate_schema.py
@@ -0,0 +1,76 @@
+import json
+from pathlib import Path
+from typing import Any, Dict, Union
+
+import yaml
+
+from jsonschema.exceptions import ValidationError
+from jsonschema.validators import validate
+
+schema_path = Path(__file__).resolve().parent.parent / "schema"
+
+
+def load_schema(schema_path: Union[str, Path]) -> Union[Dict[str, Any], str]:
+    """Load a schema from a file or directory"""
+    if Path(schema_path).is_dir():
+        return {f.stem: load_schema(f) for f in Path(schema_path).iterdir()}
+    elif Path(schema_path).is_file() and (
+        str(schema_path).endswith(".yaml") or str(schema_path).endswith(".yml")
+    ):
+        with open(schema_path, "r") as f:
+            return yaml.safe_load(f)
+    else:
+        with open(schema_path, "r") as f:
+            return f.read()
+
+
+def derefence_schema(schema):
+    """Dereference a schema by replacing $ref with the actual schema it points to"""
+
+    def _dereference_obj(obj):
+        if isinstance(obj, dict):
+            out = dict()
+            for k, v in obj.items():
+                if k == "$ref":
+                    address = v.split(".")
+                    here = schema
+                    for part in address:
+                        here = here[part]
+                    if isinstance(here, dict):
+                        out.update(_dereference_obj(here))
+                    else:
+                        return _dereference_obj(here)
+                else:
+                    out[k] = _dereference_obj(v)
+            return out
+        if isinstance(obj, list):
+            return [_dereference_obj(x) for x in obj]
+        return obj
+
+    return _dereference_obj(schema)
+
+
+def validate_schema(schema: dict):
+    schema = derefence_schema(schema)
+    with open("metaschema.json", "r") as f:
+        metaschema = json.load(f)
+
+    # validate is put in this try/except clause because the error is sometimes too long to
+    # print in the terminal
+    try:
+        validate(instance=schema, schema=metaschema)
+    except ValidationError as e:
+        with open("error_log.txt", "w") as file:
+            file.write(str(e))
+            raise e
+
+
+def main():
+    schema = load_schema(schema_path)
+    validate_schema(schema)
+
+
+if __name__ == "__main__":
+    main()
+
+
