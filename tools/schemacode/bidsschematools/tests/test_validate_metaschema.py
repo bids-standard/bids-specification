a9911d56d9051536e033abadd496c1f8d208507b
Ben Dichter <ben.dichter@gmail.com>
2024-04-16 19:46:39 -0500

    use existing functions

--- tools/schemacode/bidsschematools/tests/test_validate_metaschema.py
+++ tools/schemacode/bidsschematools/tests/test_validate_metaschema.py
@@ -1,46 +1,19 @@
 import pytest
 from jsonschema.exceptions import ValidationError
 
-from bidsschematools.validate_schema import (
-    dereference_schema,
-    load_schema,
-    schema_path,
-    validate_schema,
-)
-
-
-def test_load_schema():
-    """Test that a valid schema does not raise an error."""
-    load_schema(schema_path)
-
-
-def test_derefence_schema():
-    """Test that a valid schema does not raise an error."""
-    schema = load_schema(schema_path)
-    dereference_schema(schema)
+from bidsschematools.schema import load_schema
+from bidsschematools.validate_schema import validate_schema
 
 
 def test_valid_schema():
     """Test that a valid schema does not raise an error."""
-    schema = load_schema(schema_path)
+    schema = load_schema()
     validate_schema(schema)
-
-
-def test_broken_reference():
-    """Test that a broken reference raises an error."""
-    schema = load_schema(schema_path)
-    schema["rules"]["files"]["deriv"]["preprocessed_data"]["anat_nonparametric_common"][
-        "$ref"
-    ] = "broken.reference"
-    with pytest.raises(KeyError) as e:
-        dereference_schema(schema)
-
-    assert "'broken'" in str(e.value)
 
 
 def test_add_legal_field():
     """Test that adding a legal field does not raise an error."""
-    schema = load_schema(schema_path)
+    schema = load_schema()
     schema["rules"]["files"]["deriv"]["preprocessed_data"]["anat_nonparametric_common"][
         "entities"
     ]["density"] = "optional"
@@ -49,7 +22,7 @@ def test_add_legal_field():
 
 def test_invalid_value():
     """Test that an invalid value raises an error."""
-    schema = load_schema(schema_path)
+    schema = load_schema()
     schema["rules"]["files"]["deriv"]["preprocessed_data"]["anat_nonparametric_common"][
         "entities"
     ]["density"] = "invalid"
--- tools/schemacode/bidsschematools/validate_schema.py
+++ tools/schemacode/bidsschematools/validate_schema.py
@@ -1,57 +1,15 @@
 import json
 from pathlib import Path
-from typing import Any, Dict, Union
 
-import yaml
 from jsonschema.exceptions import ValidationError
 from jsonschema.validators import validate
 
-schema_path = Path("src") / "schema"
+from .schema import load_schema
+
 metaschema_path = Path("src") / "metaschema.json"
 
 
-def load_schema(schema_path: Union[str, Path]) -> Union[Dict[str, Any], str]:
-    """Load a schema from a file or directory"""
-    if Path(schema_path).is_dir():
-        return {f.stem: load_schema(f) for f in Path(schema_path).iterdir()}
-    elif Path(schema_path).is_file() and (
-        str(schema_path).endswith(".yaml") or str(schema_path).endswith(".yml")
-    ):
-        with open(schema_path, "r") as f:
-            return yaml.safe_load(f)
-    else:
-        with open(schema_path, "r") as f:
-            return f.read()
-
-
-def dereference_schema(schema: dict):
-    """Dereference a schema by replacing $ref with the actual schema it points to"""
-
-    def _dereference_obj(obj):
-        if isinstance(obj, dict):
-            out = dict()
-            for k, v in obj.items():
-                if k == "$ref":
-                    address = v.split(".")
-                    here = schema
-                    for part in address:
-                        here = here[part]
-                    if isinstance(here, dict):
-                        out.update(_dereference_obj(here))
-                    else:
-                        return _dereference_obj(here)
-                else:
-                    out[k] = _dereference_obj(v)
-            return out
-        if isinstance(obj, list):
-            return [_dereference_obj(x) for x in obj]
-        return obj
-
-    return _dereference_obj(schema)
-
-
 def validate_schema(schema: dict):
-    schema = dereference_schema(schema)
     with open(metaschema_path, "r") as f:
         metaschema = json.load(f)
 
