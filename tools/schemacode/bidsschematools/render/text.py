1afbfe8f67f2d6975c2f401b9dcde78110ed1e15
Yaroslav Halchenko <debian@onerussian.com>
2024-04-19 12:56:07 -0700

    Make extensions identifiers contain . to separate extensions where multiple used (#1755)
    * Make extensions identifiers contain . to separate extensions where multiple used
    
    * Use underscore since "." is used in path to any object
    
    * [DATALAD RUNCMD] sed -i'' -e 's/tsvgz/tsv_gz/g' src/*.md ...
    
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "sed -i'' -e 's/tsvgz/tsv_gz/g' src/*.md src/*/*.md",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^
    
    ---------
    
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>

--- tools/schemacode/bidsschematools/render/text.py
+++ tools/schemacode/bidsschematools/render/text.py
@@ -401,8 +401,8 @@ def make_filename_template(
 
             ext_headings = []
             for extension in extensions:
-                # The glossary indexes by the extension identifier (niigz instead of .nii.gz),
-                # but the rules reference the actual suffix string (.nii.gz instead of niigz),
+                # The glossary indexes by the extension identifier (nii_gz instead of .nii.gz),
+                # but the rules reference the actual suffix string (.nii.gz instead of nii_gz),
                 # so we need to look it up.
                 key = ext_key_table.get(extension)
                 if key:
