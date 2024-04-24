95ac640e1fe831230e0d65b7a40ae4db6c5fe2cf
Horea Christian <chr@chymera.eu>
2024-04-24 14:50:49 -0400

    Using clize as an alternative to argh

--- tools/schemacode/bidsschematools/__main__.py
+++ tools/schemacode/bidsschematools/__main__.py
@@ -3,34 +3,15 @@ import os
 
 from clize import run
 
-from .schema import export_schema, load_schema
+from .schema import export
+from .validator import validate_bids
 
 
 def cli():
     run({
-        'validate': ,
-        ''
+        'validate': validate_bids,
+        'export': export,
         })
-
-
-@cli.command()
-@click.option("--schema")
-@click.option("--output", default="-")
-@click.pass_context
-def export(ctx, schema, output):
-    """Export BIDS schema to JSON document"""
-    logger = logging.getLogger("bidsschematools")
-    schema = load_schema(schema)
-    text = export_schema(schema)
-    if output == "-":
-        logger.debug("Writing to stdout")
-        print(text)
-    else:
-        output = os.path.abspath(output)
-        logger.debug(f"Writing to {output}")
-        with open(output, "w") as fobj:
-            fobj.write(text)
-
 
 if __name__ == "__main__":
     cli()
