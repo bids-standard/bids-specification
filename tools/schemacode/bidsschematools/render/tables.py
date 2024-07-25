40ca7bbd2f9bd2d981df29707d8ff9f3e9ab78de
Chris Markiewicz <effigies@gmail.com>
2024-07-25 12:54:32 -0400

    feat(spec): Render definition and enum columns alike

--- tools/schemacode/bidsschematools/render/tables.py
+++ tools/schemacode/bidsschematools/render/tables.py
@@ -74,8 +74,8 @@ def _make_object_table(
         "columns": "column",
     }.get(table_type)
 
-    for element in subschema.keys():
-        field_name = subschema[element]["name"]
+    for element, field in subschema.items():
+        field_name = field["name"]
         # NOTE: Link to the glossary entry,
         # except for subobjects (if table_type) and
         # "additional columns" (field_name.startswith("**"))
@@ -90,19 +90,19 @@ def _make_object_table(
             "[DEPRECATED](SPEC_ROOT/common-principles.md#definitions)",
         )
 
-        type_string = utils.resolve_metadata_type(subschema[element])
+        type_string = utils.resolve_metadata_type(field)
 
         description = utils.normalize_requirements(
-            subschema[element]["description"] + " " + description_addendum
+            f"{field['description']} {description_addendum}".strip()
         )
 
         # Append a list of valid values, if provided, to the description.
         # If there are a lot of valid values, this will add a link to the description linking to
         # the associated glossary entry.
-        if (
-            "enum" in subschema[element].keys()
-            and len(subschema[element]["enum"]) >= n_values_to_combine
-        ):
+        levels = subschema[element].get("enum", []) or subschema[element].get(
+            "definition", {}
+        ).get("Levels", [])
+        if len(levels) >= n_values_to_combine:
             glossary_entry = f"{GLOSSARY_PATH}.md#objects.{table_type}.{element}"
             valid_values_str = (
                 f"For a list of valid values for this {element_type}, see the "
