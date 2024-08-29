bd5fde19d632dc2b216c19e403faad302acb1b5f
Chris Markiewicz <effigies@gmail.com>
2024-08-29 16:48:23 -0400

    Add docstring for citation/mod.ts

--- citation/mod.ts
+++ citation/mod.ts
@@ -1,1 +1,8 @@
+/**
+ * CITATION.cff schema
+ *
+ * This module exports the CITATION.cff JSON schema, downloaded from
+ * https://raw.githubusercontent.com/citation-file-format/citation-file-format/main/schema.json
+ * on 2024-08.29.
+ */
 export { default as schema } from "./schema.json" with { type: "json" };
