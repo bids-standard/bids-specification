baff2538edd24bccc684a2d8353cea50ccf77fc9
Chris Markiewicz <effigies@gmail.com>
2024-08-29 16:45:21 -0400

    Add module docstring

--- mod.ts
+++ mod.ts
@@ -1,3 +1,22 @@
+/**
+ * BIDS Schema object and types
+ *
+ * This module exports the BIDS Schema object and types for working with it.
+ *
+ * ```ts no-eval
+ * import type { Schema, Context } from 'jsr:@bids/schema'
+ * import { schema } from 'jsr:@bids/schema'
+ *
+ * const metadataFields = schema.objects.metadata
+ * const filenameRules = schema.rules.files
+ * ```
+ *
+ * The `Schema` type is derived from the JSON Schema of the BIDS Schema object.
+ * The `Context` type is derived from the `schema.meta.context` object, interpreted
+ * as a JSON Schema.
+ *
+ * Because these files are auto-generated, they have no documentation of their own.
+ */
 import type { Schema } from "./metaschema.ts";
 import { default as schemaObject } from "./schema.json" with { type: "json" };
 
