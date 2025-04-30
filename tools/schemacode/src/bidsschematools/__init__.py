67974c390b6062a6e29db5ec4d890b544cfe8f3e
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-04-29 22:43:02 -0400

    fix: Document __version__ attributes

--- tools/schemacode/src/bidsschematools/__init__.py
+++ tools/schemacode/src/bidsschematools/__init__.py
@@ -4,24 +4,32 @@
 .. autodata:: __bids_version__
 """
 
-global __version__
-global __bids_version__
+__version__: str
+__bids_version__: str
 
 __all__ = ("__version__", "__bids_version__")
 
 
-def __getattr__(attr):
+def __getattr__(attr: str) -> str:
+    """Lazily load the schema version and BIDS version from the filesystem."""
+    from typing import TypeVar
+
     from .data import load
 
-    if attr == "__version__":
-        global __version__
-        __version__ = load.readable("schema/SCHEMA_VERSION").read_text().strip()
-        __version__.__doc__ = "Schema version"
-        return __version__
-    elif attr == "__bids_version__":
-        global __bids_version__
-        __bids_version__ = load.readable("schema/BIDS_VERSION").read_text().strip()
-        __bids_version__.__doc__ = "BIDS specification version"
-        return __bids_version__
+    T = TypeVar("T")
+
+    def document(obj: T, docstring: str) -> T:
+        tp = type(obj)
+        return type(tp.__name__, (tp,), {"__doc__": docstring})(obj)
+
+    versions = {
+        "__version__": ("schema/SCHEMA_VERSION", "Schema version"),
+        "__bids_version__": ("schema/BIDS_VERSION", "BIDS specification version"),
+    }
+
+    if attr in versions:
+        resource, docstring = versions[attr]
+        globals()[attr] = document(load.readable(resource).read_text().strip(), docstring)
+        return globals()[attr]
 
     raise AttributeError(f"module {__spec__.name!r} has no attribute {attr!r}")
