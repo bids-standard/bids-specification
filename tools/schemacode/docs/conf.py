53c4bd8daaf2571fcbf42f6aaf1f0641d605c301
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-06-11 05:38:18 +0200

    doc: Suppress tests in API docs

--- tools/schemacode/docs/conf.py
+++ tools/schemacode/docs/conf.py
@@ -48,13 +48,14 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "tests"]
+exclude_patterns = ["_build"]
 
 
 autosummary_mock_imports = [
     "pytest",
     # Mock internal modules to avoid building docs
     "bidsschematools.conftest",
+    "bidsschematools.tests",
 ]
 
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
